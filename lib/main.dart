e5dabc024c32d339233a07cfd9261f9e25272e5f
myorion74 <o.orion@hotmail.com>
2024-01-26 10:48:49 +0700

    Update New
    - API
    - Generate Barcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,295 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
- 
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      
-      theme: ThemeData(
-     
-        primarySwatch: Colors.pink,
-        visualDensity: VisualDensity.adaptivePlatformDensity
-      ),
-      home: FormPage(), //MyHomePage(title: 'My Orion (2023)'),
+    return const MaterialApp(
+      home: LoginPage(),
     );
   }
 }
 
-
- 
-class MyHomePage extends StatelessWidget {
-   MyHomePage({required this.title, required this.data, required this.age, required this.menu});
-final String title;
-final String data;
-final int age;
-final List<String> menu;
-String description = "Hello Orion";
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  _LoginPageState createState() => _LoginPageState();
+}
 
-    return Scaffold(
-      appBar: AppBar(
+class _LoginPageState extends State<LoginPage> {
 
-        title: Text(title),
-      ),
-      body: Center(
-        
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              //Image.network('https://en.wikipedia.org/wiki/Buldak#/media/File:Korean_barbeque-Buldak-01.jpg'),
-              Text(data),
-              Text("อายุ $age ปี"),
-              SaveMessage(data,menu),
-            ],
-            
-     
-        ),
-      ), 
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.push(
-          context, 
-          MaterialPageRoute(builder: (context)=> Page2(description: "Hello $data",)) , 
-        ),child: const Icon(Icons.arrow_forward),
-        
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  Future<void> _login() async {
+    var host = 'ysth.intranet';
+    var login = 'yt600923'; //loginController.text;
+    String password = 'Vv44444$'; //passController.text;
+    var port = 389;
+
+    var connection = new d(
+        host: host, ssl: false, port: port, bindDN: login, password: password);
+
+    try {
+      await connection.open();
+      await connection.bind();
+      print('Bind OK');
+    } catch (e, stacktrace) {
+      print('********* Exception: $e, Stacktrace: $stacktrace');
+    } finally {
+      print('Closing');
+      await connection.close();
+    }
   }
-}
-Widget SaveMessage(String data,List<String> menu){
-  
-  return Container(
-    width: 300,
-    height: 300,
-    child: Center(
-      child: Column(
-            children: menu.map((e) => Text(e)).toList(),
-    )
-    ));
-}
 
-class FormPage extends StatefulWidget{
- @override
- _FormPageState createState () => _FormPageState();
-
-} 
-
-class _FormPageState extends State<FormPage> {
-  final _formKey = GlobalKey<FormState>();
-  late String name;
-  late String lastname;
-  late int age;
-  late String selectedSex="male";
-  late bool _isOption1=false;
-  late bool _isOption2=false;
-  late bool _isOption3=false;
-  late bool _isOption4=false;
-
-  late String errormessage="";
-
-  List<String> selectedOptions = [];
-
-void _onRadioChange(String value){
-    setState(() {
-    selectedSex = value;
-  });
-  print(value);
-}
-
-@override
-Widget build(BuildContext context){
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Form'),
-    ),
-
-     body: Center(
-       child: Container(
-        padding: EdgeInsets.all(15.0),
-        child: Form(
-          key:  _formKey,
-          child: Column(
-            children: <Widget>[
-              Text("First Name:"),
-              TextFormField(
-              initialValue: "",
-              onSaved: (value) => setState(() {
-                name = value!;
-              }), //,
-              ),
-              Text("LastName:"),
-              TextFormField(
-                initialValue: "",
-                onSaved: (value)=> setState(() {
-                  lastname = value!;
-                }),
-              ),
-              Text("Age"),
-              TextFormField(
-                initialValue: "",
-                onSaved: (value)=>setState(() {
-                  age = int.parse(value!);
-                }),
-              ),
-              Text("Gender:"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Radio(value: 'male', groupValue: selectedSex, 
-                  onChanged: (value) => _onRadioChange(value!)),
-                  Text('male'),
-                  Radio(value: 'female', groupValue: selectedSex, 
-                  onChanged: (value) => _onRadioChange(value!)),
-                  Text('female')
-                ],
-              ),
-    Column(children: [
-        CheckboxListTile(
-          title: const Text('เนื้อหมูย่าง'), 
-          value: _isOption1 , 
-          onChanged: (val) {
-            setState(() {
-              _isOption1 =!_isOption1;
-              if(_isOption1){
-                selectedOptions.add("Pork1");
-              }else{
-                selectedOptions.remove("Pork1");
-              }
-              selectedOptions.remove("Fire Rice");
-            });
-          } ),
-          CheckboxListTile(
-            title: const Text("ซีกโครงหมูย่าง"),
-            value: _isOption2, 
-            onChanged: (val)=>
-            setState(() {
-              _isOption2=!_isOption2;
-              if(_isOption2)
-              {
-                selectedOptions.add("Pork2");
-              }else{
-                selectedOptions.remove("Pork2");
-              }
-
-            })
-            ),
-            CheckboxListTile(
-              title: Text("ไก่ทอดซอสเกาหลี"),
-              value: _isOption3, 
-              onChanged: (val)=>
-              setState(() {
-                _isOption3=!_isOption3;
-                if(_isOption3)
-                {
-                  selectedOptions.add("Pork3");
-                }else
-                {
-                  selectedOptions.remove("Pork3");
-                }
-              })),
-              CheckboxListTile(
-                title: Text("ปีกไก่ทอด"),
-                value: _isOption4,
-                onChanged: (val)=>
-                setState(() {
-                  _isOption4 =!_isOption4;
-                  if(_isOption4)
-                  {
-                    selectedOptions.add("fire_chicken");
-                  }else{
-                    selectedOptions.remove("fire_chicken");
-                  }
-                })
-                )
-    ],),
-              ElevatedButton(
-              onPressed: (){
-                if(_formKey.currentState!.validate()) {
-                  //if(name!="" && lastname !="" )
-                  if(1==1)
-                  {
-                    _formKey.currentState!.save();
-                    print(name);
-                    print(selectedOptions);
-                    print('Save');
-                  
-                    Navigator.push(context,
-                    MaterialPageRoute(
-                      builder: (context)=> 
-                      MyHomePage(
-                        title: "Orders",
-                        data: "$selectedSex $name $lastname",
-                        age: age,
-                        menu: selectedOptions
-                        )));
-                  }else
-                  {
-                    errormessage ="Name or Lastname are empty";
-                     print(name);
-                      print(lastname);
-                    print("Name or Lastname are empty");
-                  }
-                }
-              }, 
-              child: Text('Save'),
-              ),
-              
-            ],
-          ),
-         ),
-       ),
-     ),
-);
-}//Scaffold
-  
-}
-
-class Page2 extends StatelessWidget {
-  final String description;
-
-  const Page2({super.key, required this.description});
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-
-        title: Text('Page2'),
-        ),
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
       body: Center(
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width:300,
-              height: 300,
-              child: Image.asset('assets/IMG_7846.jpeg')),
-            Container(
-              width: 300,
-              height: 300,
-              child: Image.network('https://www.manjarokitchen.com/wp-content/uploads/rpress/2020/05/grilled-chicken-spicy.png'),
-            ),
-            Container(
-              padding: EdgeInsets.all(15.0),
-
-              child: Text(description),
-            ), ElevatedButton(
-              onPressed: () => Navigator.pop(context), 
-              //MaterialPageRoute(builder:  (context) =>  MyHomePage(title:'Page1',)),
-              //), 
-              child: Text('Back'),),
-          ]
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: ElevatedButton(
+          onPressed: _login,
+          child: Text('Login with Active Directory'),
+        ),
       ),
     );
   }
--- main_temp.dart
+++ main_temp.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'dart:developer' as developer;
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +13,379 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+      title: 'Food Center',
 
-        primarySwatch: Colors.pink,
-        visualDensity: VisualDensity.adaptivePlatformDensity
-      ),
-      home: const MyHomePage(title: 'My Orion (2023)'),
-      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: const FormPage(), //MyHomePage(title: 'My Orion (2023)'),
     );
   }
 }
 
-
- 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+// ignore: must_be_immutable
+class MyHomePage extends StatelessWidget {
+  MyHomePage(
+      {super.key,
+      required this.title,
+      required this.data,
+      required this.age,
+      required this.menu});
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-
-  //     _counter++;
-  //   });
-  // }
+  final String data;
+  final int age;
+  final List<String> menu;
+  String description = "Hello Orion";
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Container(
-        child: Text('Page1'),
-      ), 
-        // child: Column(
-    
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: <Widget>[
-        //     Container(
-        //       padding: EdgeInsets.all(10.0),
-        //       color: Colors.yellow,
-              
-        //       child: Text("Opas Leangkosakul"),
-        //       ),
-        //      Text(
-        //       'this many times:'
-        //     ),
-        //   Text(
-        //      '[$_counter]',
-        //      style: Theme.of(context).textTheme.headlineMedium,
-        //    ),
-         // ],
-        //),
-      //),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //Image.network('https://en.wikipedia.org/wiki/Buldak#/media/File:Korean_barbeque-Buldak-01.jpg'),
+            Text(data),
+            Text("อายุ $age ปี"),
+            SaveMessage(data, menu),
+          ],
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.push(
-          context, 
-          MaterialPageRoute(builder: (context)=> Page2()) , //_incrementCounter,
-        //tooltip: 'Increment',
-        ),child: const Icon(Icons.arrow_forward),
-        
+          context,
+          MaterialPageRoute(
+              builder: (context) => Page2(
+                    description: "Hello $data",
+                  )),
+        ),
+        child: const Icon(Icons.arrow_forward),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// ignore: non_constant_identifier_names
+Widget SaveMessage(String data, List<String> menu) {
+  return SizedBox(
+      width: 300,
+      height: 300,
+      child: Center(
+          child: Column(
+        children: menu.map((e) => Text(e)).toList(),
+      )));
+}
+
+class FormPage extends StatefulWidget {
+  const FormPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _FormPageState createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  final _formKey = GlobalKey<FormState>();
+  late String name = "";
+  late String lastname = "";
+  late int age = 0;
+  late String selectedSex = "male";
+  late bool _isOption1 = false;
+  late bool _isOption2 = false;
+  late bool _isOption3 = false;
+  late bool _isOption4 = false;
+
+  late String errormessage = "";
+
+  List<String> selectedOptions = [];
+
+  void _onRadioChange(String value) {
+    setState(() {
+      selectedSex = value;
+    });
+    developer.log(
+      'onRadioChange',
+      name: '_FormPageState',
+      error: value,
+    );
+    //print(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Form'),
+        ),
+        body: Center(
+            child: Container(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+              key: _formKey,
+              child: Column(children: <Widget>[
+                const Text("First Name:"),
+                TextFormField(
+                  initialValue: "",
+                  onSaved: (value) => setState(() {
+                    name = value!;
+                  }), //,
+                ),
+                const Text("LastName:"),
+                TextFormField(
+                  initialValue: "",
+                  onSaved: (value) => setState(() {
+                    lastname = value!;
+                  }),
+                ),
+                const Text("Age"),
+                TextFormField(
+                  initialValue: "",
+                  onSaved: (value) => setState(() {
+                    age = int.parse(value!);
+                  }),
+                ),
+                const Text("Gender:"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Radio(
+                        value: 'male',
+                        groupValue: selectedSex,
+                        onChanged: (value) => _onRadioChange(value!)),
+                    const Text('male'),
+                    Radio(
+                        value: 'female',
+                        groupValue: selectedSex,
+                        onChanged: (value) => _onRadioChange(value!)),
+                    const Text('female')
+                  ],
+                ),
+                Column(children: [
+                  QrImageView(
+                    data: '0818118145',
+                    version: QrVersions.auto,
+                    size: 200.0,
+                  ),
+                ]),
+                Column(
+                  children: [
+                    CheckboxListTile(
+                        title: const Text('เนื้อหมูย่าง'),
+                        value: _isOption1,
+                        onChanged: (val) {
+                          setState(() {
+                            _isOption1 = !_isOption1;
+                            if (_isOption1) {
+                              selectedOptions.add("Pork1");
+                            } else {
+                              selectedOptions.remove("Pork1");
+                            }
+                            selectedOptions.remove("Fire Rice");
+                          });
+                        }),
+                    CheckboxListTile(
+                        title: const Text("ซีกโครงหมูย่าง"),
+                        value: _isOption2,
+                        onChanged: (val) => setState(() {
+                              _isOption2 = !_isOption2;
+                              if (_isOption2) {
+                                selectedOptions.add("Pork2");
+                              } else {
+                                selectedOptions.remove("Pork2");
+                              }
+                            })),
+                    CheckboxListTile(
+                        title: const Text("ไก่ทอดซอสเกาหลี"),
+                        value: _isOption3,
+                        onChanged: (val) => setState(() {
+                              _isOption3 = !_isOption3;
+                              if (_isOption3) {
+                                selectedOptions.add("Pork3");
+                              } else {
+                                selectedOptions.remove("Pork3");
+                              }
+                            })),
+                    CheckboxListTile(
+                        title: const Text("ปีกไก่ทอด"),
+                        value: _isOption4,
+                        onChanged: (val) => setState(() {
+                              _isOption4 = !_isOption4;
+                              if (_isOption4) {
+                                selectedOptions.add("fire_chicken");
+                              } else {
+                                selectedOptions.remove("fire_chicken");
+                              }
+                            }))
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    developer.log("check validate");
+                    if (_formKey.currentState!.validate()) {
+                      //if(name!="" && lastname !="" )
+                      if (1 == 1) {
+                        _formKey.currentState!.save();
+
+                        //developer.log(name);
+                        //developer.log(selectedOptions.toString());
+                        developer.log("Save");
+                        //print(name);
+                        //print(selectedOptions);
+                        //print('Save');
+
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MyHomePage(
+                                    title: "Orders",
+                                    data: "$selectedSex $name $lastname",
+                                    age: age,
+                                    menu: selectedOptions)));
+                      } else {
+                        //errormessage = "Name or Lastname are empty";
+                        developer.log(
+                          'onRadioChange',
+                          name: '_FormPageState',
+                          error: "$name $lastname is Empty",
+                        );
+                        //print(name);
+                        //print(lastname);
+                        //print("Name or Lastname are empty");
+                      }
+                    }
+                  },
+                  child: const Text('Go'),
+                )
+              ])),
+        )));
+    //    selectedSex: selectedSex,
+    //    name: name,
+    //    lastname: lastname,
+    //    title: "MENU",
+    //    data: "",
+    //    age: age,
+    //    selectedOptions: selectedOptions),
+  } //Scaffold
 }
 
 class Page2 extends StatelessWidget {
+  final String description;
+
+  const Page2({super.key, required this.description});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-
-        title: Text('Page2'),
-        ),
-      body: Center(
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Text('Page2'),
-            ), ElevatedButton(
-              onPressed: () => Navigator.pop(context), 
-              //MaterialPageRoute(builder:  (context) =>  MyHomePage(title:'Page1',)),
-              //), 
-              child: Text('Back'),),
-          ]
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(
+        title: const Text('Page2'),
       ),
+      body: Center(
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          SizedBox(
+              width: 300,
+              height: 300,
+              child: Image.asset('assets/IMG_7846.jpeg')),
+          SizedBox(
+            width: 300,
+            height: 300,
+            child: Image.network(
+                'https://www.manjarokitchen.com/wp-content/uploads/rpress/2020/05/grilled-chicken-spicy.png'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(description),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.pop(context),
+            //MaterialPageRoute(builder:  (context) =>  MyHomePage(title:'Page1',)),
+            //),
+            child: const Text('Back'),
+          ),
+        ]), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class DialogMessage extends StatelessWidget {
+  DialogMessage(
+      {super.key,
+      required this.selectedSex,
+      required this.name,
+      required this.lastname,
+      required this.title,
+      required this.data,
+      required this.age,
+      required this.selectedOptions});
+
+  final _formKey = GlobalKey<FormState>();
+  final String selectedSex;
+  final String name;
+  final String lastname;
+  final String title;
+  final String data;
+  final int age;
+  final List<String> selectedOptions;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Confirm Ordering'),
+                  content: const Text('รายการอาหาร'),
+                  actions: <Widget>[
+                    ElevatedButton(
+                      onPressed: () => Navigator.pop(context, 'Cancel'),
+                      child: const Text('Cancel'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        developer.log("check validate");
+                        if (_formKey.currentState!.validate()) {
+                          //if(name!="" && lastname !="" )
+                          if (1 == 1) {
+                            _formKey.currentState!.save();
+
+                            //developer.log(name);
+                            //developer.log(selectedOptions.toString());
+                            developer.log("Save");
+                            //print(name);
+                            //print(selectedOptions);
+                            //print('Save');
+
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MyHomePage(
+                                        title: "Orders",
+                                        data: "$selectedSex $name $lastname",
+                                        age: age,
+                                        menu: selectedOptions)));
+                          } else {
+                            //errormessage = "Name or Lastname are empty";
+                            developer.log(
+                              'onRadioChange',
+                              name: '_FormPageState',
+                              error: "$name $lastname is Empty",
+                            );
+                            //print(name);
+                            //print(lastname);
+                            //print("Name or Lastname are empty");
+                          }
+                        }
+                      },
+                      child: const Text('Go'),
+                    )
+                  ])),
+      child: const Text('Save'),
     );
   }
 }
